c6fb0df287215816cd4eab3e98dc776a3ccfd397
Juan Rosas <jjuan_2001@hotmail.com>
2022-10-11 21:29:07 -0500

    Se añadio la clase para representar los nuevos items y se añadió la carpeta de recursos y lenguajes

---
+++ src/main/java/net/rinlax/TestMod/Item/ModItems.java
@@ -0,0 +1,21 @@
+package net.rinlax.TestMod.Item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.rinlax.TestMod.TestMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MOD_ID);
+
+    public static final RegistryObject<Item> COBALT_INGOT = ITEMS.register("cobalt_ingot", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    
+    public static final RegistryObject<Item> COBALT_NUGGET = ITEMS.register("cobalt_nugget", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
