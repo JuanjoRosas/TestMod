0fe894f27265c6e570a0214c89af26fe8f369ba4
Juan Rosas <jjuan_2001@hotmail.com>
2022-10-15 20:39:35 -0500

    recetas para los bloques e items agregados y eliminación de imports inncesarios dentro de clases item, block y testmod

--- src/main/java/net/rinlax/TestMod/Item/ModItems.java
+++ src/main/java/net/rinlax/TestMod/Item/ModItems.java
@@ -1,6 +1,5 @@
 package net.rinlax.TestMod.item;
 
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
